3e23133ab26035bda4781fa193882b6247767933
Marco Chitolina <68135699+MarcoChit0@users.noreply.github.com>
2021-08-03 20:15:52 -0300

    contact list

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:english_words/english_words.dart';
 class Contact{
   final String _name, _genre, _cellphone, _email;
   Contact(this._name, this._cellphone, this._email, this._genre);
@@ -8,18 +8,19 @@ class Contact{
   get email => this._email;
   get genre => this._genre;
 
-  List<Contact> randomList(int n) {
-    Random random = Random();
-    var genders = ['M', 'F', 'U'];
 
-    return List.generate(n, (i) => Contact(
-        'person $i',
-        [
-          List.generate(4, (index) => random.nextInt(10)).join(''), 
-          List.generate(4, (index) => random.nextInt(10)).join('')
-        ].join('-'),
-        'person$i@mail.com',
-        genders[random.nextInt(genders.length)]
-    ));
-  }
+}
+List<Contact> RandomContactList(int n) {
+  Random random = Random();
+  var genders = ['M', 'F', 'U'];
+
+  return List.generate(n, (i) => Contact(
+      generateWordPairs().take(1).toString(),
+      [
+        List.generate(4, (index) => random.nextInt(10)).join(''),
+        List.generate(4, (index) => random.nextInt(10)).join('')
+      ].join('-'),
+      'person$i@mail.com',
+      genders[random.nextInt(genders.length)]
+  ));
 }
--- lib/contact_list.dart
+++ lib/contact_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:navigation_project/contact.dart';
 // every dart program starts
 // everything in flutter is an widget
 // you build your program composing widgets
@@ -13,27 +14,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Welcome to Flutter',
-      home: RandomWords(),
+      home: RandomContacts(),
     );
   }}
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class RandomContacts extends StatefulWidget {
+  const RandomContacts({Key? key}) : super(key: key);
 
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _RandomContactsState createState() => _RandomContactsState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};      // get users saved words
+class _RandomContactsState extends State<RandomContacts> {
+  final _suggestions = <Contact>[];
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   // build method only need to return a widget
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
+        title: const Text('Startup Contact Generator'),
       ),
       body: _buildSuggestions(),
     );
@@ -48,17 +48,16 @@ class _RandomWordsState extends State<RandomWords> {
 
           final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            _suggestions.addAll(RandomContactList(10)); /*4*/
           }
           return _buildRow(_suggestions[index]);
         });
   }
   // list style
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(Contact my_contact) {
     return ListTile(
       title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+        my_contact.name,
       ),
     );
   }
